f44df79728a46720c268e37e9efd3053345c342c
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2025-06-11 23:51:26 +0530

    chore ; update

--- llm_logic/task_8/final_logic_lm.py
+++ llm_logic/task_8/final_logic_lm.py
@@ -6,10 +6,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores.faiss import FAISS
-from dotenv import load_dotenv
-
-load_dotenv()
+from langchain_community.vectorstores import Chroma
 
 api_key = os.getenv("OPENAI_API_KEY")
 client = ChatOpenAI(model="gpt-4o", api_key=api_key)
@@ -34,18 +31,19 @@ Ross, Chandler and Joey are male
 Ross, Rachel, Monica, Chandler and Joey are all friends
 
 Is the below following statements is true, false or uncertain?
-Chandler and Monica are married
+Amy is rachel's sister
 """
+# Chandler and Monica are not only married but also best friends which is similar to the relationship between phoebe and mike.
+# phoebe and mike know each other from childhood
 
-
-def setup():
-    document_loader = TextLoader("vectordb.txt")
+def vectorstore_kb_creator():
+    document_loader = TextLoader("prolog_kb_context.txt")
     documnets = document_loader.load()
     splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
     document_split = splitter.split_documents(documnets)
 
     openAIEmbeddings = OpenAIEmbeddings()
-    db = FAISS.from_documents(document_split, openAIEmbeddings)
+    db = Chroma.from_documents(document_split, openAIEmbeddings)
 
     return db.as_retriever()
 
@@ -110,7 +108,7 @@ def logic_solver(nl_problem, context):
         HumanMessage(
             content=f"""
             Given the following natural language problem, convert it into Prolog facts, rules, and query to check if the claim is true, false, or uncertain.
-            Use the following knowledge base as a reference for the syntax and structure: {context}
+            Use the following knowledge base as a reference for additional context: {context}
             
             {query_format}
             
@@ -154,9 +152,9 @@ def symbolic_formulator(llm_response):
     return facts, query
 
 
-# def prolog_kb_creater(facts, filename="output.pl"):
-    
-
+def prolog_kb_creater(facts, filename="output.pl"):
+    with open(filename, "w") as file:
+        file.write(facts)
 
 # mimic result interpreter
 def classify_result(fact_file, query):
@@ -189,7 +187,7 @@ def result_interpreter(result):
 
 
 # mimic self refinement process
-def self_refinement(nl_problem, original_response, query_result, max_count=3):
+def self_refinement(nl_problem, original_response, query_result, context, max_count=3):
     if query_result != "uncertain":
         return None, None, query_result, 0
 
@@ -208,26 +206,23 @@ def self_refinement(nl_problem, original_response, query_result, max_count=3):
 
                 But, the symbolic solver was not able to produce the correct result and outputted 'uncertain'.
                 Please revise your Prolog facts, rules, and query so that the problem can be correctly resolved using logic programming.
-
+                Use the following knowledge base as a reference for additional context: {context}
                 {query_format}
 
                 Do not include any additional explanations or comments or headings.
                 """
             ),
         ]
-
+        print(HumanMessage(content=chat_messages[1].content))
         refined_response = client.invoke(chat_messages).content
         last_response = refined_response
 
         print(f"\n Refinement Attempt completed ", refine_counter)
 
         facts, query = symbolic_formulator(refined_response)
-        print("####Self refinement:####")
         print(refined_response)
 
-        # prolog_kb_creater(facts)
-        with open('output.pl', "w") as file:
-            file.write(facts)
+        prolog_kb_creater(facts)
 
         query_result = classify_result("output.pl", query)
 
@@ -242,13 +237,11 @@ def problem_formulator(nl_problem_goal, response, context):
     if response.strip() == "Logic Program":
         llm_output = logic_solver(nl_problem_goal, context)
         facts, query = symbolic_formulator(llm_output)
-        # prolog_kb_creater(facts)
+        prolog_kb_creater(facts)
         result = classify_result("output.pl", query)
 
         if result == "uncertain":
-            r_facts, r_query, result, attempts = self_refinement(
-                nl_problem_goal, llm_output, result, max_count=6
-            )
+            r_facts, r_query, result, attempts = self_refinement(nl_problem_goal, llm_output, result, context, max_count=6)
 
             if r_facts:
                 print(f"\nRefined result", result)
@@ -272,7 +265,7 @@ def problem_formulator(nl_problem_goal, response, context):
 if __name__ == "__main__":
     response = problem_classifier(nl_problem_goal)
 
-    db = setup()
+    db = vectorstore_kb_creator()
     documents = db.get_relevant_documents(nl_problem_goal)
     context = "\n".join([d.page_content for d in documents])
 
